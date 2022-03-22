ddaac42d9668e0f9bb0556cce8eac62eeda32ccc
Pratheek-7 <pratheek.bedre.97@gmail.com>
2022-03-22 23:00:22 +0530

    about some localstorage

--- src/Components/EditProducts.js
+++ src/Components/EditProducts.js
@@ -18,182 +18,200 @@ export default function EditProducts() {
       });
     }
   };
-  const handleSubmitFormForEditBtn = ()=>{
-    let contentId = document.getElementById("contentId")
-    console.log(contentId)
-  }
+  const handleSubmitFormForEditBtn = () => {
+    let allInputsInTheEditProducts = document.querySelectorAll(".bgColor");
+    let inputEditProduct = [];
+    for (let i = 0; i < allInputsInTheEditProducts.length; i++) {
+      inputEditProduct.push(allInputsInTheEditProducts[i].value);
+    }
+    let inputValues = localStorage.getItem("inputValues");
+    let inputEditProductObj;
+    if (inputValues == null) {
+      inputEditProductObj = [];
+    } else {
+      inputEditProductObj = JSON.parse(inputValues);
+    }
+    inputEditProductObj.push(inputEditProduct);
+    localStorage.setItem("inputValues", JSON.stringify(inputEditProductObj));
+  };
   return (
     <>
-    <div onMouseMove={handleOnBlur} className="container my-3">
-      <h2>Edit the rank or product here</h2><hr />
-      <form className="row my-3 g-3">
-        <div className="col-md-4">
-          <label htmlFor="validationCustom02" className="form-label">
-            Rank
-          </label>
-          <input
-            type="text"
-            className="form-control bgColor"
-            id="rankInputId"
-            required
-          />
-          <div className="invalid-feedback">Looks good!</div>
-        </div>
-        <div className="col-md-6">
-          <label htmlFor="validationServer03" className="form-label">
-            Model name
-          </label>
-          <input
-            type="text"
-            className="form-control bgColor"
-            id="validationServer03"
-            aria-describedby="validationServer03Feedback"
-            required
-          />
-          <div id="validationServer03Feedback" className="invalid-feedback">
-            Please provide a valid city.
+      <div onMouseMove={handleOnBlur} className="container my-3">
+        <h2>Edit the rank or product here</h2>
+        <hr />
+        <form className="row my-3 g-3">
+          <div className="col-md-4">
+            <label htmlFor="validationCustom02" className="form-label">
+              Rank
+            </label>
+            <input
+              type="text"
+              className="form-control bgColor"
+              id="rankInputId"
+              required
+            />
+            <div className="invalid-feedback">Looks good!</div>
           </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Image url
-          </label>
-          <input
-            type="email"
-            className="form-control bgColor"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="validationServer03Feedback" className="invalid-feedback">
-            Please provide at least one image url
+          <div className="col-md-6">
+            <label htmlFor="validationServer03" className="form-label">
+              Model name
+            </label>
+            <input
+              type="text"
+              className="form-control bgColor"
+              id="validationServer03"
+              aria-describedby="validationServer03Feedback"
+              required
+            />
+            <div id="validationServer03Feedback" className="invalid-feedback">
+              Please provide a valid city.
+            </div>
           </div>
-        </div>
-        {/* <center>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Image url
+            </label>
+            <input
+              type="email"
+              className="form-control bgColor"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="validationServer03Feedback" className="invalid-feedback">
+              Please provide at least one image url
+            </div>
+          </div>
+          {/* <center>
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQogQ3P3ojYSeiQf-5SlBmN0VxGePAsk8WpmQ&usqp=CAU"
             className="card-img-top d-block w-50"
             alt="..."
           ></img>
         </center> */}
-        <ProductPhoto
-          imageUrl1="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe2YNPbIkb2o-v4Vg6CJx4bDhAEFmwIfSGCw&usqp=CAU"
-          imageUrl2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQogQ3P3ojYSeiQf-5SlBmN0VxGePAsk8WpmQ&usqp=CAU"
-          imageUrl3="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbRyzBd6jnUbcZD5DbMyPR3qJOAjel21xiEw&usqp=CAU"
-        />
-        <div className="col-md-6">
-          <label htmlFor="validationServer03" className="form-label">
-            Price
-          </label>
-          <input
-            type="text"
-            className="form-control bgColor"
-            id="validationServer03"
-            aria-describedby="validationServer03Feedback"
-            required
+          <ProductPhoto
+            imageUrl1="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe2YNPbIkb2o-v4Vg6CJx4bDhAEFmwIfSGCw&usqp=CAU"
+            imageUrl2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQogQ3P3ojYSeiQf-5SlBmN0VxGePAsk8WpmQ&usqp=CAU"
+            imageUrl3="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbRyzBd6jnUbcZD5DbMyPR3qJOAjel21xiEw&usqp=CAU"
           />
-          <div id="validationServer03Feedback" className="invalid-feedback">
-            Please provide a valid city.
-          </div>
-        </div>
-        <hr />
-        <h3>Add specifications</h3>
-        <div className="col-md-4">
-          <label htmlFor="validationCustom01" className="form-label">
-            Display
-          </label>
-          <input
-            type="text"
-            className="form-control bgColor"
-            id="validationCustom01"
-            required
-          />
-          <div className="invalid-feedback">Looks good!</div>
-        </div>
-        <div className="col-md-4">
-          <label htmlFor="validationCustom02" className="form-label">
-            Chip
-          </label>
-          <input
-            type="text"
-            className="form-control bgColor"
-            id="validationCustom02"
-            required
-          />
-          <div className="invalid-feedback">Looks good!</div>
-        </div>
-        <div className="col-md-4">
-          <label htmlFor="validationCustom02" className="form-label">
-            Battery and Power
-          </label>
-          <input
-            type="text"
-            className="form-control bgColor"
-            id="validationCustom02"
-            required
-          />
-          <div className="invalid-feedback">Looks good!</div>
-        </div>
-        <div className="col-md-4">
-          <label htmlFor="validationCustom02" className="form-label">
-            Memory
-          </label>
-          <input
-            type="text"
-            className="form-control bgColor"
-            id="validationCustom02"
-            required
-          />
-          <div className="invalid-feedback">Looks good!</div>
-        </div>
-        <div className="col-md-4">
-          <label htmlFor="validationCustom02" className="form-label">
-            Storage
-          </label>
-          <input
-            type="text"
-            className="form-control bgColor"
-            id="validationCustom02"
-            required
-          />
-          <div className="invalid-feedback">Looks good!</div>
-        </div>
-        <div className="col-md-4">
-          <label htmlFor="validationCustom02" className="form-label">
-            Camera
-          </label>
-          <input
-            type="text"
-            className="form-control bgColor"
-            id="validationCustom02"
-            required
-          />
-          <div className="invalid-feedback">Looks good!</div>
-        </div>
-        <hr />
-        <div className="col-12">
-          <div className="form-check">
+          <div className="col-md-6">
+            <label htmlFor="validationServer03" className="form-label">
+              Price
+            </label>
             <input
-              className="form-check-input is-invalid"
-              type="checkbox"
-              value=""
-              id="invalidCheck3"
-              aria-describedby="invalidCheck3Feedback"
+              type="text"
+              className="form-control bgColor"
+              id="validationServer03"
+              aria-describedby="validationServer03Feedback"
               required
             />
-            <label className="form-check-label" htmlFor="invalidCheck3">
-              Agree to terms and conditions
-            </label>
-            <div id="invalidCheck3Feedback" className="invalid-feedback">
-              You must agree before submitting.
+            <div id="validationServer03Feedback" className="invalid-feedback">
+              Please provide a valid city.
             </div>
           </div>
-        </div>
-      </form>
+          <hr />
+          <h3>Add specifications</h3>
+          <div className="col-md-4">
+            <label htmlFor="validationCustom01" className="form-label">
+              Display
+            </label>
+            <input
+              type="text"
+              className="form-control bgColor"
+              id="validationCustom01"
+              required
+            />
+            <div className="invalid-feedback">Looks good!</div>
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="validationCustom02" className="form-label">
+              Chip
+            </label>
+            <input
+              type="text"
+              className="form-control bgColor"
+              id="validationCustom02"
+              required
+            />
+            <div className="invalid-feedback">Looks good!</div>
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="validationCustom02" className="form-label">
+              Battery and Power
+            </label>
+            <input
+              type="text"
+              className="form-control bgColor"
+              id="validationCustom02"
+              required
+            />
+            <div className="invalid-feedback">Looks good!</div>
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="validationCustom02" className="form-label">
+              Memory
+            </label>
+            <input
+              type="text"
+              className="form-control bgColor"
+              id="validationCustom02"
+              required
+            />
+            <div className="invalid-feedback">Looks good!</div>
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="validationCustom02" className="form-label">
+              Storage
+            </label>
+            <input
+              type="text"
+              className="form-control bgColor"
+              id="validationCustom02"
+              required
+            />
+            <div className="invalid-feedback">Looks good!</div>
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="validationCustom02" className="form-label">
+              Camera
+            </label>
+            <input
+              type="text"
+              className="form-control bgColor"
+              id="validationCustom02"
+              required
+            />
+            <div className="invalid-feedback">Looks good!</div>
+          </div>
+          <hr />
+          <div className="col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input is-invalid"
+                type="checkbox"
+                value=""
+                id="invalidCheck3"
+                aria-describedby="invalidCheck3Feedback"
+                required
+              />
+              <label className="form-check-label" htmlFor="invalidCheck3">
+                Agree to terms and conditions
+              </label>
+              <div id="invalidCheck3Feedback" className="invalid-feedback">
+                You must agree before submitting.
+              </div>
+            </div>
+          </div>
+        </form>
         <div className="col-12">
-          <Link className="btn btn-primary" onClick={handleSubmitFormForEditBtn} to="/">
+          <Link
+            className="btn btn-primary"
+            onClick={handleSubmitFormForEditBtn}
+            to="/"
+          >
             Submit form
           </Link>
         </div>
-    </div></>
+      </div>
+    </>
   );
 }
