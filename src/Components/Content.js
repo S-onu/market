a56828af5744879a1c3929e6127d30595aaa008f
Pratheek-7 <pratheek.bedre.97@gmail.com>
2022-03-22 23:26:51 +0530

    finally done by the help of localstorage

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -2,14 +2,24 @@ import React from "react";
 import Products from "./Products";
 
 export default function Content() {
+  let inputValues = localStorage.getItem("inputValues");
+  let inputEditProductObj;
+  if (inputValues == null) {
+    inputEditProductObj = [];
+  } else {
+    inputEditProductObj = JSON.parse(inputValues);
+  }
+  console.log(inputEditProductObj);
   return (
-    <div className="container">
-      <div id="contentId" className="my-3">
-        <Products/>
-        {/* <Products/> */}
-        {/* <Products/> */}
-        {/* <Products/> */}
+    <>
+      <div className="container">
+        <div id="contentId" className="my-3">
+          <Products />
+          {inputEditProductObj.map(() => {
+            return <Products />;
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
