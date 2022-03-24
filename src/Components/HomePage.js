d8cf4b3db9e7e58de94ed3c556255dcbd2bb34a1
Pratheek-7 <pratheek.bedre.97@gmail.com>
2022-03-24 19:01:43 +0530

    something added

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -15,7 +15,7 @@ export default function HomePage() {
         />
       </div>
       <div className="container py-2 my-4 bg-dark">
-        <h4>Upto 55% off</h4>
+        <h4 className="mx-1">Upto 55% off</h4>
         <div className="row1" style={{ display: "flex", overflowX: "scroll" }}>
           <CardsInRows imageUrlForCard="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC14bUcYj0MG4evp8IiAaBm2ytz2XDYaA_Bw&usqp=CAU" />
           <CardsInRows imageUrlForCard="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC_Nd0xiyrN9Z48TMmglVBYhRouqzzVp9SJQ&usqp=CAU" />
