59475ccea1672fc02275f9687a55156e610bf22a
Pratheek-7 <pratheek.bedre.97@gmail.com>
2022-03-27 18:50:39 +0530

    added

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,19 +3,9 @@ import Products from "./Products";
 // import Source from "./Components/source.json";
 
 export default function Content(props) {
-  // localStorage.clear();
-  // let laptop = [];
-  // let mobile = [];
   let inputValues = localStorage.getItem("inputValues");
   let inputEditProductObj;
-  // if (inputValues == null) {
-    // inputEditProductObj = [laptop, mobile];
-  // } else {
-    inputEditProductObj = JSON.parse(inputValues);
-  // }
-  // inputEditProductObj[0] = Source.articles1
-  // inputEditProductObj[1] = Source.articles2
-  // localStorage.setItem("inputValues", JSON.stringify(inputEditProductObj));
+  inputEditProductObj = JSON.parse(inputValues);
   let index = -1;
   // Remember that local storage should not be cleared
   return (
@@ -24,7 +14,13 @@ export default function Content(props) {
         <div id="contentId" className="my-3">
           {inputEditProductObj[props.state].map(() => {
             index++;
-            return <Products runFunc={props.runFunc} state0={props.state} NoOfProducts={index} />;
+            return (
+              <Products
+                runFunc={props.runFunc}
+                state0={props.state}
+                NoOfProducts={index}
+              />
+            );
           })}
         </div>
       </div>
