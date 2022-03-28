5a4dea837a5c230b76c70ca9e2daf75e7e2af510
Pratheek-7 <pratheek.bedre.97@gmail.com>
2022-03-28 11:03:54 +0530

    added

--- src/Components/LeftFooterComponent.js
+++ src/Components/LeftFooterComponent.js
@@ -80,35 +80,6 @@ export default function LeftFooterComponent(props) {
             aria-expanded="false"
             style={{ backgroundColor: "#273034" }}
           >
-            Within price range
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li>
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="dropdown mx-2">
-          <button
-            className="btn btn-secondary dropdown-toggle"
-            type="button"
-            id="dropdownMenuButton1"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-            style={{ backgroundColor: "#273034" }}
-          >
             Shop in
           </button>
           <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
